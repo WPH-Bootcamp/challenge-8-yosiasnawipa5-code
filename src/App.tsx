2c1fb9216b5c016413147d31ec86c43472183712
yosiasnawipa5-code <yosiasnawipa5@gmail.com>
2026-05-25 16:07:25 +0000

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-
-import Home from "./pages/Home"
-import Navbar from "./components/layout/Navbar"
-
 function App() {
   return (
     <div className="min-h-screen bg-slate-50 text-slate-900">
-      <Navbar />
-      <Home />
-    
+   
     </div>
   )
 }
