4296eea23ed8493e2909eef7ee188e9d85304e62
yosiasnawipa5-code <yosiasnawipa5@gmail.com>
2026-05-25 15:23:18 +0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-/**
- * Main App Component
- *
- * TODO untuk mentee:
- * 1. Import Home page component (setelah dibuat)
- * 2. Render Home component di sini
- * 3. Atau setup routing jika membuat multiple pages
- *
- * Current: Placeholder untuk testing Tailwind setup
- */
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import HomePage from "./pages/HomePage";
