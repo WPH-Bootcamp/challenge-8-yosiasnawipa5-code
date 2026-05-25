18b2913d2fe0e3f0c5957644cba0b0b154595452
yosiasnawipa5-code <yosiasnawipa5@gmail.com>
2026-05-25 11:36:25 +0700

    Add barrel export for data modules
    Re-export data from multiple files in the index.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,1 +1,7 @@
+// barrel export
 
+//file ini hanya re-export semua data dari masing-masing file.
+export * from "./navigation";
+export * from "./services";
+export * from "./testimonials";
+export * from "./company";
