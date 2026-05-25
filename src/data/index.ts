fc760b7788a433d7cbd381b1321b9974ec310c96
yosiasnawipa5-code <yosiasnawipa5@gmail.com>
2026-05-25 13:19:21 +0700

    Delete src/data/index.ts

--- src/data/index.ts
+++
@@ -1,7 +0,0 @@
-// barrel export
-
-//file ini hanya re-export semua data dari masing-masing file.
-export * from "./navigation";
-export * from "./services";
-export * from "./testimonials";
-export * from "./company";
